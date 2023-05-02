9624fc5ce80ed95fb3cee8757762bedbaaf4ec65
IvaylaAtanasova <ivaapostolova@abv.bg>
2023-05-02 14:26:21 +0300

    uploaded exerc2

---
+++ exerc2.js
@@ -0,0 +1,4 @@
+let num1 = parseFloat(prompt("Enter the first number:"));
+let num2 = parseFloat(prompt("Enter the second number:"));
+let result = num1 + num2;
+console.log(`The sum of ${num1} and ${num2} is ${result}.`);
